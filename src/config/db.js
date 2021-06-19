8696417ddfebe43b4d270f97b27032e0ce1493fc
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-06-19 16:53:47 -0300

    conexão separada

--- src/config/db.js
+++ src/config/db.js
@@ -4,12 +4,18 @@ if(process.env.NODE_ENV !== "production"){
 
 const mysql = require('mysql2');
 const util = require('util');
-console.log("-------------")
-console.log(process.env.DATABASE_URL)
-const connectionString = process.env.DATABASE_URL;
+// console.log("-------------")
+// console.log(process.env.DATABASE_URL)
+// const connectionString = ;
 const db = {};
 db.exec = async (sql, values) => {
-  const conn = mysql.createConnection(connectionString);
+  const conn = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE,
+    port: process.env.DATABASE_PORT,
+  });
   const query = util.promisify(conn.query).bind(conn);
   try {
     const rows = await query(sql, values);
